ddc322903912a68d3d4216cfd92924306cfe6d9c
Sparkjd <spark.justdev@gmail.com>
2019-03-14 21:36:31 +0900

    yoda float component generalize

--- projects/yoda-float/src/lib/yoda-float-ship/yoda-float-ship.component.ts
+++ projects/yoda-float/src/lib/yoda-float-ship/yoda-float-ship.component.ts
@@ -1,38 +1,78 @@
 import {
   Component, OnInit, ViewChild, ViewContainerRef,
   ComponentFactoryResolver, Type, ComponentRef,
-  AfterViewInit, Input, Renderer2
+  AfterViewInit, Input, Renderer2, AfterContentChecked, ChangeDetectorRef
 } from '@angular/core';
 import { YodaFloatService, YodaFloatRef } from '../yoda-float.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'yoda-float-ship',
   templateUrl: './yoda-float-ship.component.html',
-  styleUrls: ['./yoda-float-ship.component.css']
+  styleUrls: ['./yoda-float-ship.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        // transform: 'rotateY(0) translateZ(0)'
+        // transform: 'translateX(0)',
+        filter: 'blur(0)',
+        opacity: 1
+      })),
+      state('inactive', style({
+        opacity: 0,
+        filter: 'blur(4px)',
+        // transform: 'translateX(200px)',
+        // transform: 'rotateY(90deg)'
+      })),
+      transition('active => inactive', animate('200ms ease-out')),
+      transition('inactive => active', animate('200ms ease-in'))
+    ])
+  ]
 })
-export class YodaFloatShipComponent<T> implements YodaFloatRef<T>, AfterViewInit, OnInit {
+export class YodaFloatShipComponent<T> implements YodaFloatRef<T>, AfterContentChecked, AfterViewInit, OnInit {
   @ViewChild('vc', { read: ViewContainerRef }) vc: ViewContainerRef;
   @Input() c: Type<T>;
   needScroll = true;
   selfRef: ComponentRef<YodaFloatShipComponent<T>>;
   instance: T;
   ref: ComponentRef<T>;
+  flip = 'inactive';
+  isFlipped = false;
+  isVisible = false;
+
   constructor(
     private fr: ComponentFactoryResolver,
     private renderer2: Renderer2,
-    private yodaFloatService: YodaFloatService
+    private yodaFloatService: YodaFloatService,
+    private cdf: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
+
   }
 
   destroy() {
-    if (this.selfRef) {
-      this.selfRef.destroy();
-    }
+    this.flip = 'inactive';
+    setTimeout(() => {
+      if (this.ref) {
+        this.ref.destroy();
+      }
+      if (this.selfRef) {
+        this.selfRef.destroy();
+      }
+    }, 200);
   }
 
   ngAfterViewInit(): void {
+    if (!this.isVisible) {
+      this.isVisible = true;
+      this.flip = 'active';
+      this.cdf.detectChanges();
+      setTimeout(() => {
+        this.isFlipped = true;
+        // this.renderer2.setStyle(this.selfRef.location.nativeElement, 'perspective', '0');
+      }, 200);
+    }
     if (this.needScroll) {
       setTimeout(() => {
         this.needScroll = false;
@@ -43,12 +83,11 @@ export class YodaFloatShipComponent<T> implements YodaFloatRef<T>, AfterViewInit
     }
   }
 
-  init(selfRef: ComponentRef<YodaFloatShipComponent<T>>, c: Type<T>, size?: string | number): YodaFloatShipComponent<T> {
-    const factory = this.fr.resolveComponentFactory(c);
-    this.selfRef = selfRef;
-    this.ref = this.vc.createComponent(factory);
-    this.instance = this.ref.instance;
-    this.renderer2.addClass(selfRef.location.nativeElement, 'layout-item');
+  ngAfterContentChecked() {
+
+  }
+
+  setSize(size: string | number) {
     let sizeWidth = '500px';
     if (size) {
       if (typeof size === 'string') {
@@ -62,7 +101,18 @@ export class YodaFloatShipComponent<T> implements YodaFloatRef<T>, AfterViewInit
         sizeWidth = `${size}px`;
       }
     }
-    this.renderer2.setStyle(selfRef.location.nativeElement, 'min-width', sizeWidth);
+    this.renderer2.setStyle(this.selfRef.location.nativeElement, 'min-width', sizeWidth);
+    this.renderer2.setStyle(this.selfRef.location.nativeElement, 'max-width', sizeWidth);
+  }
+
+  init(selfRef: ComponentRef<YodaFloatShipComponent<T>>, c: Type<T>, size?: string | number): YodaFloatShipComponent<T> {
+    const factory = this.fr.resolveComponentFactory(c);
+    this.selfRef = selfRef;
+    this.ref = this.vc.createComponent(factory);
+    this.instance = this.ref.instance;
+    this.renderer2.addClass(selfRef.location.nativeElement, 'layout-item');
+    // this.renderer2.setStyle(selfRef.location.nativeElement, 'perspective', '800px');
+    this.setSize(size);
     return this;
   }
 }
--- projects/yoda-float/src/lib/yoda-float.module.ts
+++ projects/yoda-float/src/lib/yoda-float.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { YodaFloatComponent } from './yoda-float.component';
 import { YodaFloatService } from './yoda-float.service';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { YodaFloatShipComponent } from './yoda-float-ship/yoda-float-ship.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { YodaFloatShipComponent } from './yoda-float-ship/yoda-float-ship.compon
     YodaFloatShipComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
   ],
   entryComponents: [
     YodaFloatShipComponent,
